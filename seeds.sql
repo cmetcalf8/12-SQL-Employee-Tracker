e8a046a41e0d76b6cf4ae3c172cd83e1a296c5ff
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-08-31 20:27:48 -0600

    finished seeds

--- seeds.sql
+++ seeds.sql
@@ -6,4 +6,15 @@ VALUES
 ('Operations');
 
 INSERT INTO role (title, salary, department_id)
-VALUES('Full Stack Developer', 80000, 1),
+VALUES
+('Full Stack Developer', 70000, 1),
+('Financial Analyst', 120000, 2),
+('Sales Manager', 150000, 3),
+('Project Manager', 80000, 4),
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Dennis', 'Reynolds', 3, 3)
+('Dee', 'Reynolds', 4, null)
+('Ronald', 'MacDonald', 2, null)
+('Charlie', 'Kelly', 1, 1)
