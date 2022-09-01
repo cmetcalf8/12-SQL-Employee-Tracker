a2952697cefbab69a5363f6c8afe843a27e6e60a
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-08-31 20:14:52 -0600

    starting seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (name)
+VALUES
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Full Stack Developer', 80000, 1),
